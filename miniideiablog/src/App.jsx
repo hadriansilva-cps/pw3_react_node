45ef9bd118ca827a3dddc723e74be90b63f0e927
danmacedo-cps <danilo.macedo@etec.sp.gov.br>
2024-08-19 20:42:05 -0300

    Aula 0819 -Instalação da Biblioteca MUI React

--- miniideiablog/src/App.jsx
+++ miniideiablog/src/App.jsx
@@ -1,13 +1,22 @@
-
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import ResponsiveAppBar from './Navbar/NavBar';
 import './App.css'
+import Typography from '@mui/material/Typography';
 
 function App() {
-
   return (
     <>
-     <div>
-      <h1>Minha página em React</h1>
-     </div>
+      <div>
+        <ResponsiveAppBar />
+        <Box sx={{ width: '90%', maxWidth: 500 }}>
+          <Typography variant="h4" gutterBottom>
+            Minha página em React
+          </Typography>
+          <Button variant="contained">Hello world</Button>
+        </Box>
+      </div>
     </>
   )
 }
