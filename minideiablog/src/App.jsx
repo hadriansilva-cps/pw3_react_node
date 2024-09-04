c3dfe297ebdbbd268c021d2134117ead5f8f0654
hadriansilva-cps <hadrian.silva01@etec.sp.gov.br>
2024-09-04 20:43:54 -0300

    Aula 0904 Estrutura de projeto React

--- minideiablog/src/App.jsx
+++ minideiablog/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import footer from './components/footer'
 function App() {
   return (
     <>
       <div>
         <NavBar />
+        <Footer />
       </div>
     </>
   )
 }
 
-export default App
+export default footer
---
+++ minideiablog/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './footer.module.css'
+
+const footer = () => {
+  return (
+    <footer className={style.footer}>
+        <h3>ideias de quem gosta de ficar com o pé na estrada!</h3>
+        <p>ideiasApp &copy; 2024</p>
+    </footer>
+  )
+}
+
+export default footer
